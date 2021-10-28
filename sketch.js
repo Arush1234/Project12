90106c2d6e828973b75b8c5ac19210de8198eafb
Arush1234 <80717916+Arush1234@users.noreply.github.com>
2021-10-27 20:53:02 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function preload(){
+  //pre-load images
+}
+//moving backward
+path=createSprite(200,200)
+path.addImage(pathImg);
+path.velocityY=4;
+path.scale=1.2;
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+}
+
+function draw() {
+  background(0);
+
+}
